1919fa4093cd9497e16b2fd68ab27462a1335d76
Anurag Rajawat <anuragsinghrajawat22@gmail.com>
2023-02-27 11:16:27 +0530

    Rename middleware
    Signed-off-by: Anurag Rajawat <anuragsinghrajawat22@gmail.com>

--- backend/routes/todo.go
+++ backend/routes/todo.go
@@ -1,16 +1,16 @@
 package routes
 
 import (
-	"github.com/anurag-rajawat/todo-list/backend/middleware"
+	"github.com/anurag-rajawat/todo-list/backend/handlers"
 	"github.com/gorilla/mux"
 )
 
 func Router() *mux.Router {
 	router := mux.NewRouter()
-	router.HandleFunc("/todo-completed", middleware.GetCompletedTodos).Methods("GET")
-	router.HandleFunc("/todo-incomplete", middleware.GetInCompletedTodos).Methods("GET")
-	router.HandleFunc("/todo", middleware.CreateItem).Methods("POST")
-	router.HandleFunc("/todo/{id}", middleware.UpdateItem).Methods("POST")
-	router.HandleFunc("/todo/{id}", middleware.DeleteItem).Methods("DELETE")
+	router.HandleFunc("/todo-completed", handlers.GetCompletedTodos).Methods("GET")
+	router.HandleFunc("/todo-incomplete", handlers.GetInCompletedTodos).Methods("GET")
+	router.HandleFunc("/todo", handlers.CreateItem).Methods("POST")
+	router.HandleFunc("/todo/{id}", handlers.UpdateItem).Methods("POST")
+	router.HandleFunc("/todo/{id}", handlers.DeleteItem).Methods("DELETE")
 	return router
 }
